9adc2717ac9219d5fa6dcfb34e5321650fc971cb
ricardo-ave-gonzalez <ricardo.ave.gonzalez@gmail.com>
2022-05-14 00:16:55 -0300

    stack overflow error por toString

--- src/main/java/ar/com/eduit/curso/java/web/repositories/rest/ArticuloRepository.java
+++ src/main/java/ar/com/eduit/curso/java/web/repositories/rest/ArticuloRepository.java
@@ -1,30 +1,23 @@
 package ar.com.eduit.curso.java.web.repositories.rest;
 
 import ar.com.eduit.curso.java.web.entities.Articulo;
-import ar.com.eduit.curso.java.web.entities.Cliente;
 import ar.com.eduit.curso.java.web.repositories.interfaces.I_ArticuloRepository;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 
 public class ArticuloRepository implements I_ArticuloRepository {
 
     private String url;
-    //private EntityManagerFactory emf;
-
-    @PersistenceContext(unitName = "Articulo")
     private EntityManager em;
+
+    public ArticuloRepository() {
+    }
 
     public ArticuloRepository(EntityManager em) {
         this.em = em;
-    }
-
-    public ArticuloRepository() {
     }
 
     public ArticuloRepository(String url) {
@@ -57,11 +50,45 @@ public class ArticuloRepository implements I_ArticuloRepository {
 
     @Override
     public List<Articulo> getAll() {
-        String query = "SELECT a FROM Articulo a";
-        List<Articulo> articulos = em.createQuery(query).getResultList();
-        return articulos;
-    }    
-    
+        List<Articulo> list = new ArrayList();
+        try {
+            list = (List<Articulo>) em.createNamedQuery("Articulo.findAll").getResultList();
+            list = new Gson()
+                    .fromJson(Client.getResponse(url + "/all"),
+                            new TypeToken<List<Articulo>>() {
+                            }.getType());
+
+            em.close();
+        } catch (Exception e) {
+            System.out.println("*********************************************");
+            System.out.println(e);
+            System.out.println("*********************************************");
+        }
+        return list;
+    }
+
+    @Override
+    public List<Articulo> getLikeDescripcion(String descripcion) {
+        List<Articulo> list = new ArrayList();
+        if (descripcion == null) {
+            return list;
+        }
+        try {
+
+            list = (List<Articulo>) em.createNamedQuery("Alumno.findAll").getResultList();
+            em.close();
+            list = new Gson()
+                    .fromJson(Client.getResponse(url + "/likeDescripcion?descripcion=" + descripcion),
+                            new TypeToken<List<Articulo>>() {
+                            }.getType());
+        } catch (Exception e) {
+            System.out.println("*********************************************");
+            System.out.println(e);
+            System.out.println("*********************************************");
+        }
+        return list;
+    }
+
     @Override
     public Articulo getById(int id) {
         Articulo articulo = new Articulo();
